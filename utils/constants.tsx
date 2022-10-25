4bf4bfff578b4f5ae0d1dd69dcb40459de358120
Luke_425 <garcia.luc66@gmail.com>
2022-10-25 20:45:50 +0200

    Dependencies setup and navbar+sidebar components created

---
+++ utils/constants.tsx
@@ -0,0 +1,42 @@
+import { BsCode, BsEmojiSunglasses } from 'react-icons/bs';
+import { GiCakeSlice, GiGalaxy, GiLipstick } from 'react-icons/gi';
+import { FaPaw, FaMedal, FaGamepad } from 'react-icons/fa';
+
+export const topics = [
+  {
+    name: 'programming',
+    icon: <BsCode />,
+  },
+  {
+    name: 'humour',
+    icon: <BsEmojiSunglasses />,
+  },
+  {
+    name: 'gaming',
+    icon: <FaGamepad />,
+  },
+  {
+    name: 'food',
+    icon: <GiCakeSlice />,
+  },
+  {
+    name: 'dance',
+    icon: <GiGalaxy />,
+  },
+  {
+    name: 'fashion',
+    icon: <GiLipstick />,
+  },
+  {
+    name: 'animals',
+    icon: <FaPaw />,
+  },
+  {
+    name: 'sports',
+    icon: <FaMedal />,
+  },
+];
+
+export const footerList1 = ['About', 'Newsroom', 'Store', 'Contact', 'Careers', 'ByteDance', 'Creator Directory']
+export const footerList2 = ['CourteClip for Good', 'Advertise', 'Developers', 'Transparency', 'CourteClip Rewards']
+export const footerList3 = ['Help', 'Safety', 'Terms', 'Privacy', 'Creator Portal', 'Community Guidelines']
