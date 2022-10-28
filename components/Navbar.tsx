bcf069a0ddc9a8330524de6f36bc9113e0f5601e
Luke_425 <garcia.luc66@gmail.com>
2022-10-28 15:33:41 +0200

    Google authentication login/logout implemented

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -11,10 +11,13 @@ import { BiSearch } from 'react-icons/bi'
 import { IoMdAdd } from 'react-icons/io'
 
 import Logo from '../utils/logo.png'
+import { createOrGetUser } from '../utils'
+
+import useAuthStore from '../store/authStore'
 
 const Navbar = () => {
 
-  const user = false
+  const { userProfile, addUser, removeUser } = useAuthStore()
 
   return (
     <div className='w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4'>
@@ -31,12 +34,35 @@ const Navbar = () => {
 
       <div>SEARCH</div>
 
-      <div>
-        {user ? (
-          <div>Logged in</div>
-        ) : (
-          <GoogleLogin onSuccess={(response) => { }} onError={() => console.log('Error')} />
-        )}
+      <div>{userProfile ? (
+        <div className='flex gap-5 md:gap-10'>
+          <Link href='/upload'>
+            <button className='border-2 px-2 md:px-4 text-md font-semibold flex items-center gap-2'>
+              <IoMdAdd className='text-xl' /> {` `}
+              <span className='hidden md:block'>Upload</span>
+            </button>
+          </Link>
+
+          {userProfile.image && (
+            <Link href='/'>
+              <>
+                <Image
+                  width={40}
+                  height={40}
+                  className="rounded-full cursor-pointer"
+                  src={userProfile.image}
+                  alt="Profile Photo"
+                />
+              </>
+            </Link>
+          )}
+          <button type='button' className="px-2" onClick={() => { googleLogout(); removeUser(); }} >
+            <AiOutlineLogout color='red' fontSize={25} />
+          </button>
+        </div>
+      ) : (
+        <GoogleLogin onSuccess={(response) => { createOrGetUser(response, addUser) }} onError={() => console.log('Error')} />
+      )}
       </div>
     </div>
   )
